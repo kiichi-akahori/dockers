12553aabdb3845e69a222160901bc524feba38c7
akahori <51304379+kiichi-akahori@users.noreply.github.com>
2020-02-07 12:03:53 +0900

    fix docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dc(){ docker run --rm -v "/var/run/docker.sock:/var/run/docker.sock" -v $PWD:$PWD -w $PWD docker/compose:1.25.0-rc2 "$*"; }
+dc(){ docker run --rm -v "/var/run/docker.sock:/var/run/docker.sock" -v $PWD:$PWD -w $PWD docker/compose:1.25.0-rc2 "$@"; }
